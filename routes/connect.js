5770be15051302635ffb85328baaaa0c651d03ac
Brian Melton <brianmel@microsoft.com>
2015-10-30 11:49:15 -0700

    Moving auth popup close logic closer to the action

--- routes/connect.js
+++ routes/connect.js
@@ -6,8 +6,18 @@ router.get('/azure', passport.authenticate('azure'));
 
 router.get('/azure/callback',
   passport.authenticate('azure', {
-    successRedirect: '/close',
-    failureRedirect: '/error'
+    successRedirect: '/connect/close',
+    failureRedirect: '/connect/error'
   }));
+
+router.get('/close', function (req, res) {
+  res.render('auth_complete');
+});
+
+router.get('/error', function (req, res) {
+  // TODO prompt them to report an issue on Github
+  res.status(500);
+  res.send('An unexpected error was encountered.');
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/close', function (req, res) {
-  res.render('auth_complete');
-});
-
-router.get('/error', function (req, res) {
-  // TODO prompt them to report an issue on Github
-  res.status(500);
-  res.send('An unexpected error was encountered.');
-});
-
 module.exports = router;
