c1413295fd943bb3d555078630ffcd68ca30d506
Brian Melton <brianmel@microsoft.com>
2015-10-30 12:39:44 -0700

    Revert "Not using favicon stuff" npm seems to complain
    This reverts commit 8d1743c0314fdc580440dde23266df72283a1092.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
   , path = require('path')
+  , favicon = require('serve-favicon')
   , logger = require('morgan')
   , cookieParser = require('cookie-parser')
   , bodyParser = require('body-parser')
@@ -26,6 +27,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
