c689c1f090b51d792ab1ea7946002bd1ec66e6b0
Brian Melton <brianmel@microsoft.com>
2015-11-04 15:02:07 -0800

    Bugfix for header-modification exception being thrown

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
   console.log("Requestor: [" + req.sessionID + "]");
-  console.log("Cookies: " + req.cookies);
 });
 
 module.exports = router;
