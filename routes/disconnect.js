f3186e3dc51a09cd5a104dd3ec6ea7f7b2bf4ad6
Ricardo Loo <ricardol@microsoft.com>
2015-11-09 14:17:16 -0800

    Redirected the user to the azure logout endpoint

--- routes/disconnect.js
+++ routes/disconnect.js
@@ -22,17 +22,19 @@ var express = require('express')
 //   io.to(decodedNodeCookie).emit('init', 'Private socket session established');
 // });
 
-router.get('/azure', function(req, res){
+router.get('/azure', function(req, res) {
   // Destroy the cookies related to the service
   
   // Update the document in the DB
   
   // Redirect to 
-  // Get the full URL of the connect.php to send it to the logout endpoint
-  var appUrl = req.protocol + '://' + req.get('host');
-  console.log('App URL: ' + appUrl);
-  var redirect = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https%3A%2F%2Flocalhost%3A3000'; 
-  console.log(redirect);
+  // Get the full URL of root to send it to the logout endpoint
+  var appUrl =  encodeURIComponent(req.protocol + '://' + req.get('host'));
+  var redirectUrl = 
+  'https://login.microsoftonline.com/common/oauth2/logout' + 
+  '?post_logout_redirect_uri=' + appUrl; 
+  console.log(redirectUrl);  
+  res.redirect(redirectUrl);
 });
 
 module.exports = router;
