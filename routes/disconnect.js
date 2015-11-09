59add2b59648d18c50a7213ff026388b1e718d6f
Ricardo Loo <ricardol@microsoft.com>
2015-11-09 15:34:48 -0800

    Updated the document in the database to remove the provider

--- routes/disconnect.js
+++ routes/disconnect.js
@@ -26,16 +26,34 @@ router.get('/azure', function(req, res) {
   // In some apps, you'd have to delete objects that you have stored
   // in the session object. This is not the case of this sample. 
   
-  // Update the document in the DB
+  // Get a temporary user object from the request
+  var updatedUser = req.user;
   
-  // Redirect to 
-  // Get the full URL of root to send it to the logout endpoint
-  var appUrl =  encodeURIComponent(req.protocol + '://' + req.get('host'));
-  var redirectUrl = 
-  'https://login.microsoftonline.com/common/oauth2/logout' + 
-  '?post_logout_redirect_uri=' + appUrl; 
-  console.log(redirectUrl);  
-  res.redirect(redirectUrl);
+  // Remove the azure provider from the user object
+  for(var i = 0; i < updatedUser.providers.length; i++) {
+    if(updatedUser.providers[i].providerName === 'azure') {
+      updatedUser.providers.splice(i, 1);
+      break;
+    }
+  }
+  
+  // Remove the azure provider from the document
+  dbHelper.insertDoc(updatedUser, null, function(err, body) {
+    if(body.ok) {
+      // Update the current user object in the request
+      updatedUser._rev = body.rev;
+      req.user = updatedUser;
+      
+      // Get the full URL of root to send it to the logout endpoint
+      var appUrl =  encodeURIComponent(req.protocol + '://' + req.get('host'));
+      var redirectUrl = 
+      'https://login.microsoftonline.com/common/oauth2/logout' + 
+      '?post_logout_redirect_uri=' + appUrl; 
+      res.redirect(redirectUrl);
+    } else {
+      console.log('Error updating document: ' + err);
+    }
+  });
 });
 
 module.exports = router;
