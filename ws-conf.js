a3172bba8f8c7a1c75ead7deb13f0e2ad0955d8d
Ricardo Loo <ricardol@microsoft.com>
2015-12-01 11:09:05 -0800

    Updated tenant-specific endpoints to 'common'

--- ws-conf.js
+++ ws-conf.js
@@ -1,6 +1,6 @@
 exports.azureConf = {
-    authorizationURL: 'https://login.microsoftonline.com/ed46058a-1957-405e-8d5a-fae110f41cb8/oauth2/authorize',
-    tokenURL: 'https://login.microsoftonline.com/ed46058a-1957-405e-8d5a-fae110f41cb8/oauth2/token',
+    authorizationURL: 'https://login.microsoftonline.com/common/oauth2/authorize',
+    tokenURL: 'https://login.microsoftonline.com/common/oauth2/token',
     clientID: '656bfacf-5d64-418d-97dd-963e72d413de',
     clientSecret: 'OXr1ELYueLaVGTFmfsZjoUO37pCYNSqKTG0iOnJXcDA=',
     resource: 'https://graph.microsoft.com',
@@ -9,8 +9,8 @@ exports.azureConf = {
 };
 
 exports.googleConf = {
-    clientID: '117889876896-kaof7v5q7ojbna4b9r2tn2hjfq7rq3gl.apps.googleusercontent.com',
-    clientSecret: 'JB4MzfhANwhphySLpQUQhavv',
+    clientID: '966609587541-c3j11ichfkpue01hv3ghqo2hj0la8aab.apps.googleusercontent.com',
+    clientSecret: '3XzN7EhwX5tVa29w_E5hTxvh',
     callbackURL: "https://localhost:3000/connect/auth/google/callback",
     passReqToCallback: true
 };
