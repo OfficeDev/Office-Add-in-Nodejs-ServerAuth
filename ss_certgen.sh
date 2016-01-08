2207cc329da869a43fe9362575b5a196346ce4d0
Ricardo Loo <ricardol@microsoft.com>
2016-01-08 13:39:37 -0800

    Added an if statement to check for MINGW

--- ss_certgen.sh
+++ ss_certgen.sh
@@ -1,9 +1,22 @@
-openssl req \
-    -new \
-    -newkey rsa:2048 \
-    -days 365 \
-    -nodes \
-    -x509 \
-    -subj "/CN=localhost" \
-    -keyout server.key \
-    -out server.crt
+if [[ uname == *"MINGW"* ]]
+then
+    openssl req \
+        -new \
+        -newkey rsa:2048 \
+        -days 365 \
+        -nodes \
+        -x509 \
+        -subj "//CN=localhost" \
+        -keyout server.key \
+        -out server.crt
+else
+    openssl req \
+        -new \
+        -newkey rsa:2048 \
+        -days 365 \
+        -nodes \
+        -x509 \
+        -subj "/CN=localhost" \
+        -keyout server.key \
+        -out server.crt
+fi
