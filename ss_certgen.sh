18c5ec64a1826b6b57f761f3712ab08ee4b2c4d0
Brian Melton <brianmel@microsoft.com>
2016-01-08 12:06:59 -0800

    easier run

--- ss_certgen.sh
+++ ss_certgen.sh
@@ -1,65 +1,9 @@
-#!/usr/bin/env bash
-
-##
- # Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
- # See LICENSE in the project root for license information.
- ##
-
-OUT=.tmp;
-CERT=server.crt
-KEY=server.key
-CSR=server.csr;
-
-# Complexity
-CMP=2048
-
-# Valid
-DAYS=365
-
-# Cleans up any old runs
-function init {
-  clean;
-  [[ -f $CERT ]] && rm "$CERT";
-  [[ -f $KEY ]] && rm "$KEY";
-  mkdir $OUT;
-}
-
-# Generates a private key
-function gen_pk {
-  openssl genrsa -des3 -out $OUT/$KEY $CMP;
-}
-
-# Generates a Certificate Signing Request
-function gen_csr {
-  openssl req -new -key $OUT/$KEY -out $OUT/$CSR;
-}
-
-# Remove pass-prompt from key
-function strip_pass {
-  cp $OUT/$KEY $OUT/$KEY.org;
-  openssl rsa -in $OUT/$KEY.org -out $OUT/$KEY;
-}
-
-# Generate cert
-function gen_cert {
-  openssl x509 -req -days $DAYS -in $OUT/$CSR -signkey $OUT/$KEY -out $OUT/$CERT;
-}
-
-# Copies the completed cert up
-function cpup {
-  cp $OUT/$KEY $OUT/$CERT .;
-}
-
-# Cleans up this run
-function clean {
-  rm -rfv $OUT;
-}
-
-## Main
-   init \
-&& gen_pk \
-&& gen_csr \
-&& strip_pass \
-&& gen_cert \
-&& cpup \
-&& clean;
+openssl req \
+    -new \
+    -newkey rsa:2048 \
+    -days 365 \
+    -nodes \
+    -x509 \
+    -subj "/CN=localhost" \
+    -keyout server.key \
+    -out server.crt
