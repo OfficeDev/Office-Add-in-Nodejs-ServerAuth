edb127a19b8dba2067c2eaaf69eb81714829f8cb
Ricardo Loo <ricardol@microsoft.com>
2016-02-14 15:19:31 -0800

    Using a generic route to disconnect all providers

--- routes/disconnect.js
+++ routes/disconnect.js
@@ -5,67 +5,35 @@
 
 var express = require('express')
   , router = express.Router()
-  , passport = require('passport')
-  , io = require('../app')
-  , cookie = require('cookie')
-  , cookieParser = require('cookie-parser');
+  , io = require('../app');
 var dbHelper = new(require('../db/dbHelper'))();
 
-function disconnectService(user, serviceName) {
-  // Remove the supplied service from the provided user
-  for (var ii = 0; ii < user.providers.length; ii++) {
-    if (user.providers[ii].providerName === serviceName) {
-      user.providers.splice(ii, 1);
-      console.log('\n\nuser coming back: ' + JSON.stringify(user));
-      return user;
+function getLogoutUrl(req) {
+    var providerLogoutEndpoint;
+    var disconnectCompleteUrl = encodeURIComponent(req.protocol + '://' + req.get('host') + '/disconnect/' + req.params.providerName + '/complete/' + req.params.sessionID);
+    switch (req.params.providerName) {
+        case 'google':
+            providerLogoutEndpoint = 'https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=';
+            break;
+        case 'azure':
+            providerLogoutEndpoint = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=';
+            break;
     }
-  }
-  console.log('\n\nskipped user: ' + JSON.stringify(user));
-  return user;
+    return providerLogoutEndpoint + disconnectCompleteUrl;
 }
 
-function getDisconnectCompleteUrl(req, service, sessionID) {
-    return encodeURIComponent(req.protocol + '://' + req.get('host') + '/disconnect/' + service + '/complete/' + sessionID);
-}
-
-router.get('/google/complete/:sessionID', function (req, res) {
-    io.to(req.params.sessionID).emit('disconnect_complete', 'google');
+router.get('/:providerName/complete/:sessionID', function (req, res) {
+    io.to(req.params.sessionID).emit('disconnect_complete', req.params.providerName);
     res.render('disconnect_complete');
 });
 
-router.get('/google/:sessionID', function (req, res) {
+router.get('/:providerName/:sessionID', function (req, res) {
     dbHelper.deleteAccessToken (
         req.params.sessionID,
-        'google',
+        req.params.providerName,
         function (error) {
             if (error === null) {
-                var appUrl = getDisconnectCompleteUrl(req, 'google', req.params.sessionID);
-                console.log('Disconnect URL: ' + appUrl);
-                var logoutUrl = 'https://www.google.com/accounts/Logout'
-                + '?continue=https://appengine.google.com/_ah/logout?continue='
-                + appUrl;
-                res.redirect(logoutUrl);
-            }
-        }
-    );
-});
-
-router.get('/azure/complete/:sessionID', function (req, res) {
-    io.to(req.params.sessionID).emit('disconnect_complete', 'azure');
-    res.render('disconnect_complete');
-});
-
-router.get('/azure/:sessionID', function (req, res) {
-    dbHelper.deleteAccessToken (
-        req.params.sessionID,
-        'azure',
-        function (error) {
-            if (error === null) {
-                var appUrl = getDisconnectCompleteUrl(req, 'azure', req.params.sessionID);
-                console.log('Disconnect URL: ' + appUrl);
-                var logoutUrl = 'https://login.microsoftonline.com/common/oauth2/logout' +
-                '?post_logout_redirect_uri=' + appUrl;
-                res.redirect(logoutUrl);
+                res.redirect(getLogoutUrl(req));
             }
         }
     );
